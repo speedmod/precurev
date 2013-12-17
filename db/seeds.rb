bab01787a07664968e5b924d662e4cecaeb1a9e7
odawara <odawara@hyakuren.org>
2013-12-18 01:37:38 +0900

    データ修正（307曲目を追加）

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,49 @@
 # coding: utf-8
 
-# 一時的にコメントアウト
-# File.open('./db/character_link_origin.tsv').each_line do |line|
-#   song_id, chara_names = line.strip.split /\t/
-#   next unless chara_names
-#   chara_names.split(',').each do |chara_name|
-#     chara = Character.where(name: chara_name).first
-#     CharacterLink.create(song_id: song_id.to_i, character_id: chara.id) if chara
-#   end
-# end
-
-# {
-#   Composer  => './db/composer_uniq.txt',
-#   Writer    => './db/writer_uniq.txt',
-#   Vocalist  => './db/vocal_uniq.txt',
-#   Arranger  => './db/arrangers_uniq.txt',
-#   Character => './db/characters_uniq.txt'
-# }.each do |clazz, filename|
+# # シリーズデータを作成
+# # まず削除
+# Series.delete_all
+# # ファイルのデータを読み込んで作成
+# { Series => './db/series.txt' }.each do |clazz, filename|
 #   file = File.open filename
 #   begin
-#     file.each_line { |line| clazz.create(name: line.strip) }
+#     file.each_line do |line|
+#       values = line.strip.split /\t/
+#       clazz.create(id: values[0], name: values[1], year: values[2])
+#     end
 #   ensure
 #     file.close
 #   end
 # end
 
-# 一時的にコメントアウト
+# ボーカル、キャラクター、作曲、作詞、編曲の各データを作成
+# まず削除
+Composer.delete_all
+Writer.delete_all
+Vocalist.delete_all
+Arranger.delete_all
+Character.delete_all
+
+# ファイルのデータを読み込んで作成
+{
+  Composer  => './db/composer_uniq.txt',
+  Writer    => './db/writer_uniq.txt',
+  Vocalist  => './db/vocal_uniq.txt',
+  Arranger  => './db/arrangers_uniq.txt',
+  Character => './db/characters_uniq.txt'
+}.each do |clazz, filename|
+  file = File.open filename
+  begin
+    file.each_line { |line| clazz.create(name: line.strip) }
+  ensure
+    file.close
+  end
+end
+
+# # Kanaのレコードを作成
+# # 削除
+# Kana.delete_all
+# # ファイルのデータを読み込んで作成
 # {
 #   Kana  => './db/kana.tsv'
 # }.each do |clazz, filename|
@@ -40,70 +58,69 @@
 #   end
 # end
 
-# 一時的にコメントアウト
-# { Series => './db/series.txt' }.each do |clazz, filename|
-#   file = File.open filename
-#   begin
-#     file.each_line do |line|
-#       values = line.strip.split /\t/
-#       clazz.create(id: values[0], name: values[1], year: values[2])
-#     end
-#   ensure
-#     file.close
-#   end
-# end
+# ボーカル、キャラクター、作曲、作詞、編曲の各リンククラスのレコードを作成
+# 削除
+ComposerLink.delete_all
+WriterLink.delete_all
+VocalistLink.delete_all
+ArrangerLink.delete_all
+CharacterLink.delete_all
 
-# { Song  => './db/songs.tsv' }.each do |clazz, filename|
-#   file = File.open filename
-#   begin
-#     file.each_line do |line|
-#       values = line.strip.split /\t/
-#       clazz.create(id: values[0], name: values[1], series_id: values[2])
-#     end
-#   ensure
-#     file.close
-#   end
-# end
+# ファイルのデータを読み込んで作成
+[
+  { filename:   './db/vocalist_link_origin.tsv',
+    data_class: Vocalist,
+    link_class: VocalistLink,
+    data_id:    :vocalist_id },
+  { filename:   './db/character_link_origin.tsv',
+    data_class: Character,
+    link_class: CharacterLink,
+    data_id:    :character_id },
+  { filename:   './db/composer_link_origin.tsv',
+    data_class: Composer,
+    link_class: ComposerLink,
+    data_id:    :composer_id },
+  { filename:   './db/writer_link_origin.tsv',
+    data_class: Writer,
+    link_class: WriterLink,
+    data_id:    :writer_id },
+  { filename:   './db/arranger_link_origin.tsv',
+    data_class: Arranger,
+    link_class: ArrangerLink,
+    data_id:    :arranger_id },
+].each do |h|
+  file = File.open(h[:filename])
+  begin
+    file.each_line do |line|
+      song_id, names = line.strip.split /\t/
+      next unless names
+      names.split(',').each do |name|
+        data = h[:data_class].where(name: name).first
+        h[:link_class].create(:song_id => song_id.to_i, h[:data_id] => data.id) if data
+      end
+    end
+  ensure
+    file.close
+  end
+end
 
-# [
-#   { filename:   './db/vocalist_link_origin.tsv',
-#     data_class: Vocalist,
-#     link_class: VocalistLink,
-#     data_id:    :vocalist_id },
-#   { filename:   './db/character_link_origin.tsv',
-#     data_class: Character,
-#     link_class: CharacterLink,
-#     data_id:    :character_id },
-#   { filename:   './db/composer_link_origin.tsv',
-#     data_class: Composer,
-#     link_class: ComposerLink,
-#     data_id:    :composer_id },
-#   { filename:   './db/writer_link_origin.tsv',
-#     data_class: Writer,
-#     link_class: WriterLink,
-#     data_id:    :writer_id },
-#   { filename:   './db/arranger_link_origin.tsv',
-#     data_class: Arranger,
-#     link_class: ArrangerLink,
-#     data_id:    :arranger_id },
-# ].each do |h|
-#   file = File.open(h[:filename])
-#   begin
-#     file.each_line do |line|
-#       song_id, names = line.strip.split /\t/
-#       next unless names
-#       names.split(',').each do |name|
-#         data = h[:data_class].where(name: name).first
-#         h[:link_class].create(:song_id => song_id.to_i, h[:data_id] => data.id) if data
-#       end
-#     end
-#   ensure
-#     file.close
-#   end
-# end
+# 曲データを作成
+# まず削除
+Song.delete_all
+# ファイルからデータを読み込んで作成
+{ Song  => './db/songs.tsv' }.each do |clazz, filename|
+  file = File.open filename
+  begin
+    file.each_line do |line|
+      values = line.strip.split /\t/
+      clazz.create(id: values[0], name: values[1], series_id: values[2])
+    end
+  ensure
+    file.close
+  end
+end
 
 # 曲データに紐づくデータを全て予め取得しておき、songs.songdataカラムに格納しておく。
-
 data_hash = {}
 Song.all.each do |song|
   {
@@ -119,6 +136,5 @@ Song.all.each do |song|
     end
     data_hash[key] = tmp
   end
-  #p data_hash
   song.update_attribute(:songdata, JSON.dump(data_hash))
 end
